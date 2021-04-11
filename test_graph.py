e66adcd9e8473b275bf02c70c600339e90373d97
Ken C <coleken98@gmail.com>
2021-04-10 22:28:03 -0400

    hunh?

--- test_graph.py
+++ test_graph.py
@@ -5,6 +5,27 @@ from sr_instance import generate_sr_instance
 from preference_graph import PreferenceGraph
 
 class TestGraph(Scene):
+    def construct(self):
+        verts = ["a", "b", "c", "d", "e", "f"]
+        preferences = {v : [] for v in verts}
+        g = PreferenceGraph(preferences)
+        self.play(*g.create())
+        self.play(*g.propose("a", "b"))
+        self.play(*g.propose("a", "c"))
+        self.play(*g.propose("a", "d"))
+        self.play(*g.propose("a", "e"))
+        self.play(*g.propose("a", "f"))
+        self.play(*g.propose("b", "e"))
+        self.wait(1)
+        self.play(*g.accept_proposal("a", "b"))
+        self.play(*g.accept_proposal("a", "c"))
+        self.play(*g.accept_proposal("a", "d"))
+        self.play(*g.reject_proposal("b", "e"))
+        self.wait(1)
+        self.play(*g.uncreate())
+        self.wait(1)
+    
+class TestGraphIrving(Scene):
     def construct(self, preferences=None):
         if not preferences:
             preferences = generate_sr_instance(['a', 'b', 'c', 'd', 'e', 'f'])
