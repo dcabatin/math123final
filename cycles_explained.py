43da0283e1b1aecfd4791f6bcf2795bc4e84ac5e
David Cabatingan <davidcabat@gmail.com>
2021-04-13 21:17:39 -0400

    fine tuning

--- cycles_explained.py
+++ cycles_explained.py
@@ -13,7 +13,7 @@ class CycleDefinition(Scene):
         self.wait(1)
         cycle = Cycle(["$a_1$", "$a_2$", "$a_3$", "$a_4$", "$a_5$", "$a_1$"], ["$b_1$", "$b_2$", "$b_3$", "$b_4$", "$b_5$", "$b_1$"], center=[0,-0.5,0])
         self.play(*[Create(a) for a in cycle.A_mobs[:-1]])
-        definition2 = Tex("with first preferences $b_1, b_2, \ldots$", color=WHITE).scale(0.8).shift(1.4 * UP)
+        definition2 = Tex("with first preferences $b_1, b_2, \ldots, b_r$", color=WHITE).scale(0.8).shift(1.4 * UP)
         self.play(Create(definition2))
         self.wait(1)
         self.play(*[Create(b) for b in cycle.B_mobs[:-1]])
@@ -23,7 +23,7 @@ class CycleDefinition(Scene):
 
         self.play(*[Create(ar) for ar in first_pref_arrows])
         self.wait(2)
-        definition3 = Tex("Where the second favorite of $a_i$ is $b_{i+1},$", color=WHITE).scale(0.8).shift(2.4 * DOWN)
+        definition3 = Tex("where the second favorite of $a_i$ is $b_{i+1},$", color=WHITE).scale(0.8).shift(2.4 * DOWN)
         self.play(Create(definition3))
         self.wait(1)
         self.play(*[Create(ar) for ar in second_pref_arrows])
@@ -41,13 +41,15 @@ class CycleDefinition(Scene):
         self.play(Create(elimination))
         self.wait(1)
 
+        self.play(*(cycle.reject(0,0) + cycle.reject(5,5)))
+        self.wait(1)
+
         elimination2 = Tex("Each $b_i$ has $a_i$ as its least favorite, so it rejects", color=WHITE).scale(0.8).shift(2.4 * DOWN)
         self.play(Create(elimination2))
         elimination3 = Tex("$a_i$ to match with $a_{i-1}$.", color=WHITE).scale(0.8).shift(2.9 * DOWN)
         self.play(Create(elimination3))
         self.wait(1)
 
-        self.play(*(cycle.reject(0,0) + cycle.reject(5,5)))
         for i in range(5):
             self.play(*cycle.reject(i+1,i+1))
             self.play(*cycle.accept(i,i+1))
